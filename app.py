f51756c24699c39157ee3320a00cd1068683accc
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-07-09 23:47:50 +0300

    Add html pages with layout and add bootstrap5 and custom css links

--- app.py
+++ app.py
@@ -1,18 +1,90 @@
 """
 starts a Flask web application
 """
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/home")
+lessons = [{
+    'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Sara',
+    'thumbnail': 'thumbnail.jpg'
+},
+]
+
+courses = [
+{
+        'name': 'Python',
+        'icon': 'python.svg',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Data Analysis',
+        'icon': 'analysis.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Machine Learning',
+        'icon': 'machine-learning.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Web Design',
+        'icon': 'web.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Blockchain',
+        'icon': 'blockchain.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Tips & Tricks',
+        'icon': 'idea.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+]
 @app.route("/")
-def hello():
-    return "<h1>Hello World</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', lessons=lessons, courses=courses)
 
 @app.route("/about")
 def about():
-	return "<h1>About Page</h1>"
+    return render_template('about.html', title="About")
+
 
 
 if __name__ == "__main__":
