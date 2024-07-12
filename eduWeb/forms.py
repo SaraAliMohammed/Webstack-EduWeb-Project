d0365f8c3e2b9a2b6f4b117ef193d874f51cdd32
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-07-12 20:35:49 +0300

    Separate tabs in dashboard to separate files and add new_lesson tab

--- eduWeb/forms.py
+++ eduWeb/forms.py
@@ -84,3 +84,7 @@ class UpdateProfileForm(FlaskForm):
                 raise ValidationError(
                     "Email already exists! Please chosse a different one"
                 )
+
+
+class NewLessonForm(FlaskForm):
+    submit = SubmitField("Lesson")
