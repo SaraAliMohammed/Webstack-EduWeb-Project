a35acc5920b76a0b71296e9378b8d17963c34c24
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-07-12 20:44:33 +0300

    use rich ckeditor to add codeSnippet, video and upload image

--- eduWeb/routes.py
+++ eduWeb/routes.py
@@ -5,7 +5,8 @@ import secrets
 from PIL import Image
 import os
 from eduWeb.models import User, Lesson, Course
-from flask import render_template, url_for, flash, redirect, request, session, abort
+from flask import render_template, url_for, flash, redirect, request, session, abort, send_from_directory
+from flask_ckeditor import upload_success, upload_fail
 from eduWeb.forms import (
     NewCourseForm,
     NewLessonForm,
@@ -50,6 +51,25 @@ def delete_picture(picture_name, path):
         os.remove(picture_path)
     except:
         pass
+
+
+@app.route('/files/<path:filename>')
+def uploaded_files(filename):
+    path = os.path.join(app.root_path, 'static/media')
+    return send_from_directory(path, filename)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files.get('upload')
+    extension = f.filename.split('.')[-1].lower()
+    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+        return upload_fail(message='File extension not allowed!')
+    random_hex = secrets.token_hex(8)
+    image_name = random_hex+extension
+    f.save(os.path.join(app.root_path, 'static/media', image_name))
+    url = url_for('uploaded_files', filename=image_name)
+    return upload_success(url, filename=image_name)
 
 
 @app.route("/")
@@ -296,3 +316,5 @@ def delete_lesson(lesson_id):
     db.session.commit()
     flash("Your lesson has been deleted!", "success")
     return redirect(url_for("user_lessons"))
+
+
