851f650e4f3ef16bf7f82e9b3d085a660afe477a
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-07-12 20:42:26 +0300

    Update and delete lesson

--- eduWeb/routes.py
+++ eduWeb/routes.py
@@ -5,102 +5,59 @@ import secrets
 from PIL import Image
 import os
 from eduWeb.models import User, Lesson, Course
-from flask import render_template, url_for, flash, redirect, request, session
-from eduWeb.forms import RegistrationForm, LoginForm, UpdateProfileForm, NewLessonForm, NewCourseForm
+from flask import render_template, url_for, flash, redirect, request, session, abort
+from eduWeb.forms import (
+    NewCourseForm,
+    NewLessonForm,
+    RegistrationForm,
+    LoginForm,
+    UpdateProfileForm,
+    LessonUpdateForm,
+)
 from eduWeb import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_modals import render_template_modal
 
 
-lessons = [{
-    'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-{'title': 'Request Library Course',
-    'course': 'Python',
-    'author': 'Omar',
-    'thumbnail': 'thumbnail.jpg'
-},
-]
-
-courses = [
-{
-        'name': 'Python',
-        'icon': 'python.svg',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-    {
-        'name': 'Data Analysis',
-        'icon': 'analysis.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-    {
-        'name': 'Machine Learning',
-        'icon': 'machine-learning.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-        {
-        'name': 'Web Design',
-        'icon': 'web.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-        {
-        'name': 'Blockchain',
-        'icon': 'blockchain.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-        {
-        'name': 'Tips & Tricks',
-        'icon': 'idea.png',
-        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
-    },
-
-]
-
-
-def save_picture(form_picture):
+def save_picture(form_picture, path, output_size=None):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_name = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/user_pics", picture_name)
-    output_size = (150, 150)
+    picture_path = os.path.join(app.root_path, path, picture_name)
     i = Image.open(form_picture)
-    i.thumbnail(output_size)
+    if output_size:
+        i.thumbnail(output_size)
     i.save(picture_path)
     return picture_name
+
+
+def get_previous_next_lesson(lesson):
+    course = lesson.course_name
+    for lsn in course.lessons:
+        if lsn.title == lesson.title:
+            index = course.lessons.index(lsn)
+            previous_lesson = course.lessons[index - 1] if index > 0 else None
+            next_lesson = (
+                course.lessons[index + 1] if index < len(course.lessons) - 1 else None
+            )
+            break
+    return previous_lesson, next_lesson
+
+
+def delete_picture(picture_name, path):
+    picture_path = os.path.join(app.root_path, path, picture_name)
+    try:
+        os.remove(picture_path)
+    except:
+        pass
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template('home.html', lessons=lessons, courses=courses)
+    lessons = Lesson.query.all()
+    courses = Course.query.all()
+    return render_template('home.html', lessons=lessons, courses=courses)
 
 
 @app.route("/about")
@@ -110,37 +67,42 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-	if current_user.is_authenticated:
-		return redirect(url_for("home"))
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user = User(
-			fname=form.fname.data,
-			lname=form.lname.data,
-		    username=form.username.data,
-			email=form.email.data,
-			password=hashed_password)
-		db.session.add(user)
-		db.session.commit()
-		flash(f"Account created successfully for {form.username.data}", "success")
-		return redirect(url_for("home"))
-	return render_template('register.html', title="Register", form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            fname=form.fname.data,
+            lname=form.lname.data,
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Account created successfully for {form.username.data}", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(email=form.email.data).first()
-		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember=form.remember.data)
-			next_page = request.args.get('next')
-			flash("You have been logged in!", "success")
-			return redirect(next_page) if next_page else redirect(url_for("home"))
-		else:
-			flash("Login Unsuccessful. Please check credentials", "danger")
-	return render_template('login.html', title="Login", form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            flash("You have been logged in!", "success")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check credentials", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 
 @app.route("/logout")
@@ -160,7 +122,9 @@ def profile():
     profile_form = UpdateProfileForm()
     if profile_form.validate_on_submit():
         if profile_form.picture.data:
-            picture_file = save_picture(profile_form.picture.data)
+            picture_file = save_picture(
+                profile_form.picture.data, "static/user_pics", output_size=(150, 150)
+            )
             current_user.image_file = picture_file
         current_user.username = profile_form.username.data
         current_user.email = profile_form.email.data
@@ -195,6 +159,10 @@ def new_lesson():
         form = "new_course_form"
 
     if form == "new_lesson_form" and new_lesson_form.validate_on_submit():
+        if new_lesson_form.thumbnail.data:
+            picture_file = save_picture(
+                new_lesson_form.thumbnail.data, "static/lesson_thumbnails"
+            )
         lesson_slug = str(new_lesson_form.slug.data).replace(" ", "-")
         course = new_lesson_form.course.data
         lesson = Lesson(
@@ -203,6 +171,7 @@ def new_lesson():
             slug=lesson_slug,
             author=current_user,
             course_name=course,
+            thumbnail=picture_file,
         )
         db.session.add(lesson)
         db.session.commit()
@@ -210,10 +179,15 @@ def new_lesson():
         return redirect(url_for("new_lesson"))
 
     elif form == "new_course_form" and new_course_form.validate_on_submit():
+        if new_course_form.icon.data:
+            picture_file = save_picture(
+                new_course_form.icon.data, "static/course_icons", output_size=(150, 150)
+            )
         course_title = str(new_course_form.title.data).replace(" ", "-")
         course = Course(
             title=course_title,
             description=new_course_form.description.data,
+            icon=picture_file,
         )
         db.session.add(course)
         db.session.commit()
@@ -230,3 +204,95 @@ def new_lesson():
         active_tab="new_lesson",
         modal=modal,
     )
+
+
+@app.route("/<string:course>/<string:lesson_slug>")
+def lesson(lesson_slug, course):
+    lesson = Lesson.query.filter_by(slug=lesson_slug).first()
+    if lesson:
+        previous_lesson, next_lesson = get_previous_next_lesson(lesson)
+    lesson_id = lesson.id if lesson else None
+    lesson = Lesson.query.get_or_404(lesson_id)
+    return render_template(
+        "lesson_view.html",
+        title=lesson.title,
+        lesson=lesson,
+        previous_lesson=previous_lesson,
+        next_lesson=next_lesson,
+    )
+
+
+@app.route("/<string:course_title>")
+def course(course_title):
+    course = Course.query.filter_by(title=course_title).first()
+    course_id = course.id if course else None
+    course = Course.query.get_or_404(course_id)
+    return render_template(
+        "course.html",
+        title=course.title,
+        course=course,
+    )
+
+
+@app.route("/courses")
+def courses():
+    courses = Course.query.all()
+    return render_template("courses.html", title="Courses", courses=courses)
+
+
+@app.route("/dashboard/user_lessons", methods=["GET", "POST"])
+@login_required
+def user_lessons():
+    return render_template(
+        "user_lessons.html", title="Your Lessons", active_tab="user_lessons"
+    )
+
+
+@app.route("/<string:course>/<string:lesson_slug>/update", methods=["GET", "POST"])
+def update_lesson(lesson_slug, course):
+    lesson = Lesson.query.filter_by(slug=lesson_slug).first()
+    if lesson:
+        previous_lesson, next_lesson = get_previous_next_lesson(lesson)
+    lesson_id = lesson.id if lesson else None
+    lesson = Lesson.query.get_or_404(lesson_id)
+    if lesson.author != current_user:
+        abort(403)
+    form = LessonUpdateForm()
+    if form.validate_on_submit():
+        lesson.course_name = form.course.data
+        lesson.title = form.title.data
+        lesson.slug = str(form.slug.data).replace(" ", "-")
+        lesson.content = form.content.data
+        if form.thumbnail.data:
+            delete_picture(lesson.thumbnail, "static/lesson_thumbnails")
+            new_picture = save_picture(form.thumbnail.data, "static/lesson_thumbnails")
+            lesson.thumbnail = new_picture
+        db.session.commit()
+        flash("Your lesson has been updated!", "success")
+        return redirect(
+            url_for("lesson", lesson_slug=lesson.slug, course=lesson.course_name.title)
+        )
+    elif request.method == "GET":
+        form.course.data = lesson.course_name.title
+        form.title.data = lesson.title
+        form.slug.data = lesson.slug
+        form.content.data = lesson.content
+    return render_template(
+        "update_lesson.html",
+        title="Update | " + lesson.title,
+        lesson=lesson,
+        previous_lesson=previous_lesson,
+        next_lesson=next_lesson,
+        form=form,
+    )
+
+
+@app.route("/lesson/<lesson_id>/delete", methods=["POST"])
+def delete_lesson(lesson_id):
+    lesson = Lesson.query.get_or_404(lesson_id)
+    if lesson.author != current_user:
+        abort(403)
+    db.session.delete(lesson)
+    db.session.commit()
+    flash("Your lesson has been deleted!", "success")
+    return redirect(url_for("user_lessons"))
